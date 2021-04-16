926c48857e49545fa73eb5700dfe72f8338e2fc0
Oksana Yurchenkovych <oksanayurchenkovych@gmail.com>
2021-04-16 13:31:11 +0300

    start files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './index.css';
+import './App.css';
+
+class App extends Component {
+    state = {};
+
+    render() {
+        return console.log('Hello');
+    }
+}
+
+export default App;
